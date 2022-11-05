a5ae42971a2c1ba86c901dfc240250b5facf21d3
Ivan Krstic <krstic8687@yahoo.com>
2022-11-05 17:51:07 +0100

    Added implementation to fetch customer by id - GET

---
+++ mssc-brewery/src/main/java/it/springframework/msscbrewery/web/services/CustomerServiceImpl.java
@@ -0,0 +1,17 @@
+package it.springframework.msscbrewery.web.services;
+
+import it.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .name("Ivan Krstic")
+                .build();
+    }
+}
