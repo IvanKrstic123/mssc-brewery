a14c90a48d0c501d17028cb317bfd775cece10c7
Ivan Krstic <krstic8687@yahoo.com>
2022-11-06 01:44:40 +0100

    Added CRUD implementation for customer

--- mssc-brewery/src/main/java/it/springframework/msscbrewery/web/services/CustomerServiceImpl.java
+++ mssc-brewery/src/main/java/it/springframework/msscbrewery/web/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package it.springframework.msscbrewery.web.services;
 
 import it.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -13,5 +15,23 @@ public class CustomerServiceImpl implements CustomerService {
                 .id(UUID.randomUUID())
                 .name("Ivan Krstic")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .name("Ivan Krstic")
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo impl - would add a real impl to update bear
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+        log.debug("Deleting a customer...");
     }
 }
